cc927064fe28d640b3da0e6cc07d7aef3bb14d5e
Jask <2713427119@qq.com>
2025-09-18 13:08:40 +0800

    Initial Commit

---
+++ Player/PlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerApp.swift
+//  Player
+//
+//  Created by jask on 2025/9/18.
+//
+
+import SwiftUI
+
+@main
+struct PlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
